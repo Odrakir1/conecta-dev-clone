5c101023c2a2b515ca3a94ab41974f9ce844cdf5
Ricardo Moura <rikardomoura1@gmail.com>
2020-09-27 21:34:41 -0300

    Sign in page | Layout finished

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,13 +1,97 @@
-import { Typography } from '@material-ui/core';
+import { Avatar, Button, Link, TextField, Typography } from '@material-ui/core';
 import React from 'react';
 import useStyles from './style';
+import {Grid,Box} from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 function SignIn(props){
 
     const styles = useStyles();
 
     return(
-      <div className={styles.container}>
+
+        <Grid container className={styles.root}>
+            <Grid 
+                container 
+                md={7}
+                direction="column"
+                justify="center"
+                alignItems="center"
+                className={styles.image}
+                item
+            >
+
+                <Typography variant='h3' className={styles.typographyTitle}>
+                    Conectando desenvolvedores de software num só lugar!
+                </Typography>
+
+                <Typography variante='body2' className={styles.typographySubTitle}>
+                    Registre-se gratuitamente e expanda suas conexões.
+                </Typography>
+            </Grid>
+
+            <Grid md={5} item>
+                <Box display="flex" flexDirection="column" alignItems="center" m="8">
+                    <Avatar className={styles.avatar}>
+                        <LockOutlinedIcon/>
+                    </Avatar>
+                    <Typography variant="h5">
+                        Acesso
+                    </Typography>
+
+                    <form className={styles.form}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                            label="Email"
+                        />
+
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="password"
+                            name="password"
+                            autoFocus
+                            label="Senha"
+                            type="password"
+                            autoComplete="current-password"
+                        />
+
+                        <Button
+                            fullWidth
+                            color="primary"
+                            variant="contained"
+                            className={styles.button}
+                        >
+                            Entrar
+                        </Button>
+
+                        <Grid container direction="column" justify="center" alignItems="center">
+                            <Grid item className={styles.link}>
+                                <Link >Esqueceu a senha?</Link>
+                            </Grid>
+
+                            <Grid item className={styles.link}>
+                                <Link >Não tem uma conta? Registre-se!</Link>
+                            </Grid>
+                        </Grid>
+
+                    </form>
+
+                </Box>
+            </Grid>  
+        </Grid>
+
+
+      /* <div className={styles.container}>
         <div className={styles.left}>
             <Typography variant='h3'>
                 Conectando desenvolvedores de software num só lugar!
@@ -25,7 +109,7 @@ function SignIn(props){
                 <input/>
             </form>
         </div>  
-      </div>  
+      </div>   */
     );
 }
 
--- src/pages/SignIn/style.js
+++ src/pages/SignIn/style.js
@@ -1,7 +1,43 @@
 import {makeStyles} from '@material-ui/core';
 
-const useStyles = makeStyles({
-    container:{
+
+const useStyles = makeStyles((theme)=>({
+    root:{
+        height: '100vh',
+    },
+    image:{
+        backgroundImage: 'url(/images/background.jpg)',
+        backgroundPosition: "center",
+        backgroundSize:"cover",
+        backgroundRepeat:"none"
+    },
+    typographyTitle:{
+        color: '#fff',
+        fontSize:"1.8rem",
+        lineHeight:"45px"
+    },
+    typographySubTitle:{
+        color: 'rgb(255,255,255,0.7)',
+        fontSize:"1rem",
+        lineHeight:"30px",
+        marginTop:"15px"
+    },
+    avatar:{
+        background: theme.palette.primary.main,
+        marginTop: theme.spacing(2)
+    },
+    form:{
+        margin: theme.spacing(4)
+    },
+    button:{
+        marginTop: theme.spacing(3)
+    },
+    link:{
+        marginTop: theme.spacing(2),
+        cursor: "pointer"
+    }
+
+   /*  container:{
         display:"flex",
         height: '100vh'
     },
@@ -22,8 +58,8 @@ const useStyles = makeStyles({
         flexDirection:"column",
         margin: "64px 32px",
         alignItems:"center"
-    }
+    } */
 
-});
+}));
 
 export default useStyles;
